26e493ff32871cf62f825dc4750e2253d7366690
m4ruus777 <193987@student.ue.wroc.pl>
2026-06-01 14:23:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -69,11 +69,11 @@ def calfp(frates, fac_rate):
 # Driver code (odpowiednik funkcji main)
 if __name__ == "__main__":
     frates = [
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 3, 0],
-        [0, 1, 0],
-        [0, 3, 0]
+        [1, 4, 6],
+        [0, 0, 6],
+        [3, 1, 0],
+        [4, 2, 2],
+        [1, 0, 0]
     ]
 
     fac_rate = 2
